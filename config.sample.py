e2ea1ca3a4beb8980aa6deba62ff969a5dd767fe
iEdon <iedonami@gmail.com>
2023-07-16 08:28:39 +0900

    fix: linux and macos dir and minor changes

---
+++ config.sample.py
@@ -0,0 +1,16 @@
+# Twitter API Config
+twitter_config = {
+    'consumer_key': '',
+    'consumer_secret': '',
+    'access_token': '',
+    'access_token_secret': '',
+    'bearer_token': ''
+}
+
+# Mastodon Config
+mastodon_config = {
+    'client_id': '',
+    'client_secret': '',
+    'access_token': '',
+    'api_base_url': 'https://'
+}
